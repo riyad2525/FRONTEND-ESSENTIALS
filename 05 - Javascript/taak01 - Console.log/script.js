69227e520cff58b9e97a921d1cb3a7e3a68e92f6
riyad2525 <riyad.mahmud@student.rocva.nl>
2020-10-24 20:32:18 +0200

    over mij

--- 05 - Javascript/taak01 - Console.log/script.js
+++ 05 - Javascript/taak01 - Console.log/script.js
@@ -1,1 +1,8 @@
-console.log ("Het werkt uitstekend!");
+console.log("Over mij")
+let mijnVoornaam = "Riyad"
+let mijAchternaam = "Mahmud"
+let mijnLeeftijd = "null"
+let ikBenStemGericht = "Ja"
+let ikMagLegaalAlcoholKopen = "null"
+let ikMagBrommerRijden = "Ja"
+let softwareOntwikkeling = "Is een erg leuke opleing."
