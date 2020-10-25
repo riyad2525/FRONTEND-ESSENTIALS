9ea121c0df07330f141003ae05e33573ec4208fe
riyad2525 <riyad.mahmud@student.rocva.nl>
2020-10-25 13:56:31 +0100

    paste String toegether

---
+++ 05 - Javascript/taak04 - String kan plakken/script.js
@@ -0,0 +1,8 @@
+console.log ("");
+let mijnVoornaam = "Riyad"
+let mijnAchternaam = "Mahmud"
+let mijnLeeftijd = "26"
+
+let volledigeZin = "Mijn naam is" +mijnVoornaam +mijnAchternaam +"en ik ben" +mijnLeeftijd +"jaar oud.";
+
+console.log ("volledigeZin");
