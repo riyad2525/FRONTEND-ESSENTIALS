4de99f052cbc562f8964a2e63673603073870754
riyad2525 <riyad.mahmud@student.rocva.nl>
2020-10-29 19:19:35 +0100

    polen

---
+++ 05 - Javascript/taak08 - To let or not to let/script.js
@@ -0,0 +1,12 @@
+let codingIsCool = 'true';
+
+/* Dit gaat niet werken. Omdat hetzelfde varable 2 verschillende boolean waarde heet!
+En 't is ook geen if - elase steatment. */
+
+console.log (`It's ${codingIsCool}`);
+
+let huidigeJaartal = '2020';
+let geboorteJaartal = '1994';
+let mijnLeeftijdIs = huidigeJaartal - geboorteJaartal;
+
+console.log (`Ik ben ${huidigeJaartal - geboorteJaartal}`);
