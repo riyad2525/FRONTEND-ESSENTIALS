bf922d14f4a7c08d0c287d588f2aacb9a2b017c0
riyad2525 <riyad.mahmud@student.rocva.nl>
2020-10-29 10:00:04 +0100

    it's working

--- 05 - Javascript/taak06 - If  something else/script.js
+++ 05 - Javascript/taak06 - If  something else/script.js
@@ -1,6 +1,6 @@
-let stopLichtKleur = rood;
+let stopLichtKleur = 'rood';
 
-if (stopLichtKleur == rood){
+if (stopLichtKleur === 'rood'){
     console.log ("Jij moet stoppen.");
 } else {
     console.log ("Je mag door rijden.");
